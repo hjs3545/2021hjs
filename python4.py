55604e1ace1f0160e000d7a85067d7fa848f8964
hjs3545 <hjs3545@gmail.com>
2021-08-10 03:06:42 +0900

    numpy  행렬

---
+++ python4.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#행렬 표현하기
+a=np.array([[1,2],[3,4]])
+b=np.array([[10,20],[30,40]])
+
+#행렬곱 아님/ 해당 위치 원소끼리 곱
+c=a*b
+print(c)
+
+#행렬곱 함수 matmul()과 @
+d=np.matmul(a,b)
+print(d)
+print(a@b)
